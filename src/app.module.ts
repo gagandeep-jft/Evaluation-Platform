a5b458bfaf0722c2f3a37dceb4995787d26be572
Gagan Deep Singh <gagan.deep@jellyfishtechnologies.com>
2022-12-23 13:58:38 +0530

    Added NestJS project template

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './users/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TestModule } from './test/test.module';
+import { QuestionsModule } from './questions/questions.module';
+import { HistoryModule } from './history/history.module';
+import { ExecutorModule } from './executor/executor.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, TestModule, QuestionsModule, HistoryModule, ExecutorModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
