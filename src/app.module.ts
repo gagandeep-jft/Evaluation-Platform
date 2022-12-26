4f4e70490bbdb4959c8eefdf80ab9030ae4d66dd
Gagan Deep Singh <gagan.deep@jellyfishtechnologies.com>
2022-12-26 12:31:02 +0530

    Added MySQL connection to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -7,6 +8,8 @@ import { TestModule } from './test/test.module';
 import { QuestionsModule } from './questions/questions.module';
 import { HistoryModule } from './history/history.module';
 import { ExecutorModule } from './executor/executor.module';
+import { User } from './users/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -15,7 +18,21 @@ import { ExecutorModule } from './executor/executor.module';
     TestModule,
     QuestionsModule,
     HistoryModule,
+    ConfigModule,
     ExecutorModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot({})],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
