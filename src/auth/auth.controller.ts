e76339fe4540ce756d4479525c6cd807990fb127
Aakash-jft <aakash.yadav@jellyfishtechnologies.com>
2022-12-26 18:05:04 +0530

    google auth setup

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport'
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+  @Get()
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) { }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this.authService.googleLogin(req)
+  }
+}
+
