2e95b95f6d246360d93e4156d1cc5b174ab9cb60
Aakash-jft <aakash.yadav@jellyfishtechnologies.com>
2022-12-26 18:07:32 +0530

    added routs for auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
+import { GoogleStrategy } from './google.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService],
+  imports:[ConfigModule.forRoot({})],
+  providers: [AuthService,GoogleStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    googleLogin(req) {
+        if (!req.user) {
+          return 'No user from google'
+        }
+        return {
+          message: 'User Info from Google',
+          user: req.user
+        }
+      }
+      hello() {
+        return "Hello"
+      }
+}
